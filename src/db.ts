bd5b23a6cdc5685a15b2c0d42b5a73951a6ece97
Prathamesh More <moreprathamesh564@gmail.com>
2025-05-27 20:21:31 +0530

    Added v1 progress

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,20 @@
 //create user models and schemas
+import mongoose, { Schema, model, mongo} from 'mongoose';
+
+mongoose.connect("mongodb+srv://mprathamesh83:more2002@cluster0.9lg0742.mongodb.net/SecondBrain") 
+
+const UserSchema = new Schema({ 
+    username: {type : String, unique : true},
+    password: String,
+})
+
+export const UserModel = model('User', UserSchema);
+
+const ContentSchema = new Schema({
+    title:String,
+    link: String,
+    tags : [{type : mongoose.Types.ObjectId, ref : "Tag"}],
+    userId : [{type : mongoose.Types.ObjectId, ref : "User", required : true}],
+})
+
+export const ContentModel = model("Content", ContentSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,88 @@
 import express from "express";
-import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { ContentModel, UserModel } from "./db"; 
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
 
 const app = express(); 
+app.use(express.json());
 
-app.get("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    //TODO: zod validation, hash the password
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.create({
+            username,
+            password
+        })   
+        res.json({
+            message: "User created successfully"
+        }) 
+    } catch (e) {
+        res.status(500).json({
+            message : "User alredy exists"
+        })
+    }
+})
+
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if(existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message : "Invalid username or password"
+        })
+    }
 
 })
 
-app.get("/api/v1/signin", (req, res) => {
-    
+app.post("/api/v1/  ", userMiddleware, async (req, res) => {
+    const link = req.body.link;
+    const type = req.body.type;
+    await ContentModel.create({
+        link,
+        type,
+        //@ts-ignore
+        userId : req.userId,
+        tags : []
+    })
+
+    res.json({
+        message : "Content added successfully"
+    })
 })
 
-app.get("/api/v1/content", (req, res) => {
-    
-})
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId;
 
-app.delete("/api/v1/content", (req, res) => {
-    
+    await ContentModel.deleteOne({
+        contentId,
+        //@ts-ignore
+        userId : req.userId
+    })
+
+    res.json({
+        message : "Content deleted successfully"
+    })
 })
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {
     
 })
+
+app.listen(3000);
